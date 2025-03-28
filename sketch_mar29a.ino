90369aec1f93f7baa09979e73d20eb7ea3db3c15
Akshad Gajbhiye <akshadgajbhiye4@gmail.com>
2025-03-29 03:04:13 +0530

    Code_WS2812_PIR_Sensor.ino

--- sketch_mar29a.ino
+++ sketch_mar29a.ino
@@ -1,10 +1,10 @@
 #include <Adafruit_NeoPixel.h>
 
-#define PIR_PIN 2         // PIR sensor ka pin
-#define LED_PIN 6         // RGB strip ka DIN pin
-#define NUM_LEDS 72       // LED strip mein 72 LEDs
+#define PIR_PIN 2         // PIR sensor pin
+#define LED_PIN 6         // RGB strip DIN pin
+#define NUM_LEDS 72       // LED strip 72 LEDs
 
-// NeoPixel object banayein
+// NeoPixel object
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 // Attractive colors array (RGB values)
@@ -18,42 +18,40 @@ uint32_t colors[] = {
 int numColors = 5;           // Total colors in array
 
 void setup() {
-  pinMode(PIR_PIN, INPUT);      // PIR sensor ko input set karein
-  strip.begin();                // Strip initialize karein
+  pinMode(PIR_PIN, INPUT);      // PIR sensor input set
+  strip.begin();                // Strip initialize
   strip.setBrightness(255);     // Brightness full (255)
-  strip.show();                 // Shuru mein strip off karein
+  strip.show();                 // strip off
 }
 
 void loop() {
-  if (digitalRead(PIR_PIN) == HIGH) { // Motion detect hua
-    danceColors();                    // Dancing colors effect chalayein
-    clearStrip();                     // Effect ke baad strip off karein
-    delay(100);                       // Thodi der wait karein next trigger se pehle
+  if (digitalRead(PIR_PIN) == HIGH) { // if Motion detect
+    danceColors();                    // colors effect
+    clearStrip();                     // After Effect strip off
+    delay(100);                       
   } else {
-    clearStrip();                     // No motion, strip off rakhein
+    clearStrip();                     // No motion, strip off
   }
 }
 
-// Dancing colors effect function
+// colors effect function
 void danceColors() {
-  int delayTime = 9500 / NUM_LEDS; // 6.5 seconds ko LEDs ke beech divide karein (~90ms per LED)
+  int delayTime = 9500 / NUM_LEDS; // 6.5 seconds LEDs (~90ms per LED)
   
   for (int i = 0; i < NUM_LEDS; i++) {
-    // Har LED ko ek random attractive color se set karein
-    int colorIndex = random(numColors); // Random color chunein
+  
+    int colorIndex = random(numColors); // Random color
     strip.setPixelColor(i, colors[colorIndex]);
     
-    // Peeche wali LED ko fade effect ke saath off karein
     if (i > 0) {
       strip.setPixelColor(i - 1, strip.Color(0, 0, 0));
     }
     
     strip.show();
-    delay(delayTime); // Speed control karein (6.5 seconds total)
+    delay(delayTime); // Speed control (6.5 seconds total)
   }
   
-  // Last LED tak pahunchne ke baad ek final flash
-  for (int j = 0; j < 2; j++) { // 2 baar flash
+  for (int j = 0; j < 2; j++) { // 2 flash
     for (int i = 0; i < NUM_LEDS; i++) {
       strip.setPixelColor(i, colors[j % numColors]);
     }
